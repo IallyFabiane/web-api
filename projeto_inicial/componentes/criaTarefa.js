7caf104b37ebaaa63c87285d9f691cbc40ff8d30
Ially Fabiane <iallyfabiane@gmail.com>
2022-04-07 10:39:57 -0300

    refatorando o código-fonte

--- projeto_inicial/componentes/criaTarefa.js
+++ projeto_inicial/componentes/criaTarefa.js
@@ -5,7 +5,6 @@ export const handleNovoItem = (evento) => {
 
     evento.preventDefault()
     const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [] //para armazenar o dados do client-side no localStorage na forma de objeto JS
-    const lista = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
     const calendario = document.querySelector('[data-form-date]')
@@ -18,8 +17,6 @@ export const handleNovoItem = (evento) => {
     }
 
     const tarefasAtualizadas = [...tarefas, dados] // array que recebe tudo que está dentro do array tarefas e do objeto dados
-    const criaTarefa = Tarefa(dados)
-    lista.appendChild(criaTarefa)
 
     // o setItem() armazena os dados localmente
     //sessionStorage.setItem("tarefas", JSON.stringify(dados)) armazenamento dos dados recebidos do cliente na API do Navegador Chrome apenas enquanto a sessão estiver aberta(Console -> >> -> Application -> Session Storage)

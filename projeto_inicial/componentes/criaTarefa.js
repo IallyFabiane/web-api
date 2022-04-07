8b8ad78dc326d8c85910b743ba0b468e5e2d3b3d
Ially Fabiane <iallyfabiane@gmail.com>
2022-04-07 10:16:58 -0300

    refatorando o código-fonte

---
+++ projeto_inicial/componentes/criaTarefa.js
@@ -0,0 +1,44 @@
+import BotaoConclui from './concluiTarefa.js'
+import BotaoDeleta from './deletaTarefa.js'
+ 
+export const handleNovoItem = (evento) => {
+
+    evento.preventDefault()
+    const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [] //para armazenar o dados do client-side no localStorage na forma de objeto JS
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
+    const calendario = document.querySelector('[data-form-date]')
+    const data = moment(calendario.value) //LIB MOMENT
+    const dataFormatada = data.format('DD/MM/YYYY') //LIB MOMENT
+    
+    const dados = {
+        valor,
+        dataFormatada
+    }
+
+    const tarefasAtualizadas = [...tarefas, dados] // array que recebe tudo que está dentro do array tarefas e do objeto dados
+    const criaTarefa = Tarefa(dados)
+    lista.appendChild(criaTarefa)
+
+    // o setItem() armazena os dados localmente
+    //sessionStorage.setItem("tarefas", JSON.stringify(dados)) armazenamento dos dados recebidos do cliente na API do Navegador Chrome apenas enquanto a sessão estiver aberta(Console -> >> -> Application -> Session Storage)
+    localStorage.setItem("tarefas", JSON.stringify(tarefasAtualizadas)) //diferente do sessionStorage, armazena os dados mesmo após o navegador ser fechado.   
+    input.value = " "
+
+}
+
+
+export const Tarefa = ({ valor, dataFormatada }) => {
+
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`
+
+    tarefa.innerHTML = conteudo
+
+    tarefa.appendChild(BotaoConclui())
+    tarefa.appendChild(BotaoDeleta())
+    
+    return tarefa
+}
